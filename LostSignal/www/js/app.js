759c3238572ab0fa5f7bb502d657e62c204fb6ab
MGot <marcin.gotszling@gmail.com>
2015-02-26 11:44:47 +0100

    Some GUI stuff

--- LostSignal/www/js/app.js
+++ LostSignal/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('LostSignal', ['ionic', 'LostSignal.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -30,42 +30,32 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: "/search",
+  .state('app.navigate', {
+    url: "/navigate",
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
+        templateUrl: "templates/navigate.html"
       }
     }
   })
 
-  .state('app.browse', {
-    url: "/browse",
+  .state('app.heatmap', {
+    url: "/heatmap",
     views: {
       'menuContent': {
-        templateUrl: "templates/browse.html"
+        templateUrl: "templates/heatmap.html"
       }
     }
   })
-    .state('app.playlists', {
-      url: "/playlists",
+    .state('app.mainpage', {
+      url: "/mainpage",
       views: {
         'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
+          templateUrl: "templates/mainpage.html"
         }
       }
-    })
+    });
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/mainpage');
 });
